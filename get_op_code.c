b87c86eaacfd6b8af768b5e8a5309672eed27be8
Jihan Ahmed <jihanahmed41@gmail.com>
2023-12-12 07:53:29 -0800

    initial commit

---
+++ get_op_code.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+ * get_op_code - mapping between instruction and the function
+ * @ins: the passed instruction
+ *
+ * Return: a pointer to the corresponding function
+ */
+void (*get_op_code(char *ins))(stack_t **, unsigned int)
+{
+	instruction_t instructions[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (instructions[i].opcode != NULL)
+	{
+		if (strcmp(ins, instructions[i].opcode) == 0)
+			return (instructions[i].f);
+		i++;
+	}
+	return (NULL);
+}
---
+++ main.c
@@ -0,0 +1,46 @@
+#include "monty.h"
+
+/**
+ * main - the main logic of program
+ * @argc: the number of command line arguments
+ * @argv: the command line vector
+ *
+ * Return: 0 in case of success
+ */
+
+int main(int argc, char *argv[])
+{
+	int lineNo = 0;
+	FILE *fP;
+	char *line = NULL, *instr;
+	size_t n = 256;
+	ssize_t rby;
+	void (*ptrop)(stack_t **, unsigned int);
+	stack_t **top = NULL;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE); }
+	fP = fopen(argv[1], "r");
+	if (fP == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE); }
+	while (1)
+	{
+		rby = getline(&line, &n, fP);
+		if (rby == EOF || rby == '\0' || line[0] == '\n')
+			break;
+		line[strcspn(line, "\n")] = '\0';
+		lineNo++;
+		instr = strtok(line, " ");
+		ptrop = get_op_code(instr);
+		if (ptrop == NULL)
+		{
+			printf("L%d: unknown instruction %s\n", lineNo, instr);
+			exit(EXIT_FAILURE); }
+		ptrop(top, lineNo); }
+	free(line);
+	fclose(fP);
+	return (0); }
