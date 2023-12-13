171ae2f2a7062ef0b272ac88f2b92055566dbb8e
Jihan Ahmed <jihanahmed41@gmail.com>
2023-12-13 05:45:05 -0800

    updating pall function

--- op_functions.c
+++ op_functions.c
@@ -30,6 +30,8 @@ void push(stack_t **stack, unsigned int line_number)
 
 void pall(stack_t **stack, __attribute__((unused))unsigned int line_number)
 {
+	if (*stack == NULL)
+		return;
 	print_dlistint(*stack); }
 
 /**
