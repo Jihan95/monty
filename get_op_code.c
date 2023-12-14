3325eaa9680a83a3c77c403c55d6cfd0ffa52546
Jihan Ahmed <jihanahmed41@gmail.com>
2023-12-14 04:44:57 -0800

    adding sub function

--- get_op_code.c
+++ get_op_code.c
@@ -16,6 +16,7 @@ void (*get_op_code(char *ins))(stack_t **, unsigned int)
 		{"swap", swap},
 		{"add", add},
 		{"nop", nop},
+		{"sub", sub},
 		{NULL, NULL}
 	};
 	int i = 0;
--- op_functions1.c
+++ op_functions1.c
@@ -30,5 +30,24 @@ void add(stack_t **stack, unsigned int line_number)
 void nop(stack_t **stack, unsigned int line_number)
 {
 	(void) stack;
-	(void) line_number;
-}
+	(void) line_number; }
+
+/**
+ * sub - adds the top two elements of the stack.
+ * @stack: the top of the stack
+ * @line_number: the line number of the opcode
+ */
+
+void sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+	int diff = 0;
+
+	if (dlistint_len(*stack) < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exitf(stack); }
+	temp = (*stack);
+	diff = temp->n - temp->next->n;
+	temp->next->n = diff;
+	pop(stack, line_number); }
